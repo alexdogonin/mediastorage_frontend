647e8d30d3157dcea84f2b15232b580b523b99e9
svdevelop <svdevelop>
2022-03-16 18:19:57 +0300

    addFilter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
-const url = "http://192.168.0.125:8080/media";
+import "./App.scss";
+const url = "http://192.168.0.99:8080/media";
 
 function App() {
   const [photo, setPhoto] = useState(null)
@@ -16,7 +16,10 @@ function App() {
       // cache: "default",
     })
       .then(data => data.json())
-      .then(data => setPhoto(data));
+      .then(data => {
+        console.log(data)
+        setPhoto(data)
+      });
   }, []);
   console.log(photo)
 
@@ -25,6 +28,7 @@ function App() {
       {
         photo && photo.media.slice(0, 6).map(({thumb_url, uuid}, i)=> (<figure key={uuid} className={`gallery__item gallery__item--${i + 1}`}><img src={thumb_url}  className="gallery__img" alt="Image 1" /></figure>))
       }
+      <div class="grid-alpha"></div>
     </div>
   );
 }
