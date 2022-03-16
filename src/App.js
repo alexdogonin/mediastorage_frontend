488f526095e6418ac6cd119cf5678236eeae5473
svdevelop <svdevelop>
2022-03-16 15:37:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const url = "http://192.168.0.125:8080/media";
 
 function App() {
+  const [photo, setPhoto] = useState(null)
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Credentials': true
+      },
+      // mode: "cors",
+      // cache: "default",
+    })
+      .then(data => data.json())
+      .then(data => setPhoto(data));
+  }, []);
+  console.log(photo)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gallery">
+      {
+        photo && photo.media.slice(0, 6).map(({thumb_url, uuid}, i)=> (<figure key={uuid} className={`gallery__item gallery__item--${i + 1}`}><img src={thumb_url}  className="gallery__img" alt="Image 1" /></figure>))
+      }
     </div>
   );
 }
